e9cfc150a0d84748c43c470b9995280b965f4221
nickson_oinam <139377184+nickson-oinam@users.noreply.github.com>
2026-09-13 18:49:28 +0530

    Time: 2 ms (99.30%) | Memory: 44.1 MB (82.83%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            char l=s.charAt(left);
+            char r=s.charAt(right);
+
+            if(!isAlphaNum(l)){
+                left++;
+            }
+            else if(!isAlphaNum(r)){
+                right--;
+            }
+            else {
+                if(Character.toLowerCase(l)!=Character.toLowerCase(r)){
+                    return false;
+                }
+                left++;
+                right--;
+
+            }
+
+        }
+        return true;
+    }
+    private boolean isAlphaNum(char c){
+        return (c>='a'&&c<='z')||  (c>='A'&&c<='Z') || (c>='0'&&c<='9');
+    }
+}
