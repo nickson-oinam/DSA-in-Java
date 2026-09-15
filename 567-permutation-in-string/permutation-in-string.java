f653272569a175c3cc8521d3d80d52bd17a39dec
nickson_oinam <139377184+nickson-oinam@users.noreply.github.com>
2026-09-16 00:43:31 +0530

    Time: 20 ms (30.36%) | Memory: 45 MB (28.64%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,34 +1,33 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if(s1.length()>s2.length()) return false;
-        int[] freq1=new int[26];
-        int[] freq2=new int[26];
+      if(s1.length()>s2.length()) return false;
 
-        for(char c: s1.toCharArray()){
-            freq1[c-'a']++;
+      Map<Character,Integer> map=new HashMap<>();
+
+      for(char c: s1.toCharArray()){
+        map.put(c,map.getOrDefault(c,0)+1);
+      }
+    
+    int left=0;
+    int count=s1.length();
+    for(int right=0;right<s2.length();right++){
+        char r=s2.charAt(right);
+        int val=map.getOrDefault(r,0);
+        if(val>0) count--;
+        map.put(r,val-1);
+
+        if(count==0) return true;
+
+
+        if(right-left+1==s1.length()){
+            char l=s2.charAt(left);
+            int valleft=map.getOrDefault(l,0);
+            if(valleft>=0) count++;
+            map.put(l,valleft+1);
+            left++;
         }
-        int left=0;
-        for(int right=0;right<s2.length();right++){
-            char r=s2.charAt(right);
-            freq2[r-'a']++;
-            
-            if(right-left+1>s1.length()){
-                char l=s2.charAt(left);
-                freq2[l-'a']--;
-                left++;
-            }
-            if(right-left+1==s1.length()){
-                boolean ismatch=true;
-                for(int i=0;i<26;i++){
-                    if(freq1[i]!=freq2[i]){
-                         ismatch=false;
-                         break;
-                    }
-                }
-                if(ismatch) return true;
-            }
-        }
-        return false;
         
+    }
+        return false;
     }
 }
