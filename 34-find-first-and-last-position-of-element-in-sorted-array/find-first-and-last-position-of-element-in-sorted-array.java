a9ed93a1c1d52c44309bf634d8f58009df69f9b8
nickson_oinam <139377184+nickson-oinam@users.noreply.github.com>
2026-09-17 21:50:22 +0530

    Time: 0 ms (100.00%) | Memory: 48.4 MB (14.19%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -34,11 +34,12 @@ class Solution {
                 ans=mid;
                 left=mid+1;
             }
-            else if( target>nums[mid]){
-                left=mid+1;
+            else if( target<nums[mid]){
+                right=mid-1;
+                
             }
             else{
-                right=mid-1;
+                left=mid+1;
             }
         }
         return ans;
